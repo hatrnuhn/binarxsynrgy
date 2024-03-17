22b3da55fd13fad047471f640db0f3fe6cc2ae95
hatrnuhn <yusron.ibnunaim@gmail.com>
2024-03-18 05:40:26 +0700

    initial init, add hero section mobile ready

---
+++ chapter2/challenge/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      "bcr-blue": "#0D28A6",
+      "bcr-lighterblue": "#F1F3FF",
+      "bcr-limegreen": "#5CB85F",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
